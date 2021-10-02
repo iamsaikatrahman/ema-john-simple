46f17273855c52ea31127e44ca061978e8628cdb
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-10-02 16:13:51 +0600

    React Router added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>404</h2>
+            <h2>The page you are looing for is not available</h2>
+        </div>
+    );
+};
+
+export default NotFound;
