6357fe0aa3688f795357eb92dea354ef2536a715
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-10-25 13:13:39 +0600

    update

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,30 +1,45 @@
-import React from 'react';
-import useCart from '../../hooks/useCart';
-import useProducts from '../../hooks/useProducts'; 
-import { removeFromDb } from '../../utilities/fakedb';
-import Cart from '../Cart/Cart';
-import ReviewItem from '../ReviewItem/ReviewItem';
+import React from "react";
+import { useHistory } from "react-router";
+import useCart from "../../hooks/useCart";
+import useProducts from "../../hooks/useProducts";
+import { clearTheCart, removeFromDb } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const OrderReview = () => {
-    const [products, setProducts] = useProducts();
-    const [cart, setCart] = useCart(products);
-    const handleRemove = key => {
-        const newCart = cart.filter(product => product.key !== key);
-        setCart(newCart);
-        removeFromDb(key);
-    }
-    return (
-        <div className="shop-container">
-            <div className="product-container">
-                {
-                    cart.map(product => <ReviewItem key={product.key} product={product} handleRemove={handleRemove}/>)
-                }
-            </div>
-            <div className="cart-container">
-                <Cart cart={cart} />
-            </div>
-        </div>
-    );
+  const [products] = useProducts();
+  const [cart, setCart] = useCart(products);
+  const history = useHistory();
+  const handleRemove = (key) => {
+    const newCart = cart.filter((product) => product.key !== key);
+    setCart(newCart);
+    removeFromDb(key);
+  };
+  const handlePlaceOrder = () => {
+    history.push("/placeorder");
+    setCart([]);
+    clearTheCart();
+  };
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product.key}
+            product={product}
+            handleRemove={handleRemove}
+          />
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}>
+          <button onClick={handlePlaceOrder} className="btn-regular">
+            Place Order
+          </button>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default OrderReview;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,74 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import Cart from '../Cart/Cart';
-import Product from '../Product/Product';
-import {addToDb, getStoredCart} from '../../utilities/fakedb';
-import './Shop.css'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
+import "./Shop.css";
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
-    const [displayProducts, setDisplayProducts] = useState([]);
-    useEffect(()=>{
-        fetch('./products.JSON')
-        .then(res => res.json())
-        .then(data => {
-            setProducts(data);
-            setDisplayProducts(data);
-        });
-    },[])
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [displayProducts, setDisplayProducts] = useState([]);
+  useEffect(() => {
+    fetch("./products.JSON")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        setDisplayProducts(data);
+      });
+  }, []);
 
-    useEffect(()=>{
-        if(products.length){
-            const savedCart = getStoredCart();
-            const storeCart = [];
-            for(const key in savedCart){
-                const addedProduct = products.find(product => product.key === key);
-                if(addedProduct){
-                    const quantity = savedCart[key];
-                    addedProduct.quantity = quantity;
-                    storeCart.push(addedProduct);
-                }
-                setCart(storeCart);
-            }
+  useEffect(() => {
+    if (products.length) {
+      const savedCart = getStoredCart();
+      const storeCart = [];
+      for (const key in savedCart) {
+        const addedProduct = products.find((product) => product.key === key);
+        if (addedProduct) {
+          const quantity = savedCart[key];
+          addedProduct.quantity = quantity;
+          storeCart.push(addedProduct);
         }
-    },[products])
+        setCart(storeCart);
+      }
+    }
+  }, [products]);
 
-    const handleAddToCart = (product)=>{
-        const newCart = [...cart, product];
-        setCart(newCart);
-        // save to local storage
-        addToDb(product.key);
+  const handleAddToCart = (product) => {
+    const exists = cart.find((pd) => pd.key === product.key);
+    let newCart = [];
+    if (exists) {
+      const rest = cart.filter((pd) => pd.key !== product.key);
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, product];
+    } else {
+      product.quantity = 1;
+      newCart = [...cart, product];
     }
-    const handleSearch = event => {
-        const searchText =event.target.value;
-        const matchedProduct = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
-        setDisplayProducts(matchedProduct);
-    }
-    return (
-        <>
-            <div className="search-container">
-                <input 
-                type="text" 
-                onChange={handleSearch}
-                placeholder="Search Product" />
-            </div>
-            <div className="shop-container">
-                <div className="product-container">
-                    {/* <h3>Product: {products.length}</h3> */}
-                    {
-                        displayProducts.map(product => <Product 
-                            key= {product.key}
-                            product={product}
-                            handleAddToCart={handleAddToCart}
-                            ></Product>)
-                    }
-                </div>
-                <div className="cart-container">
-                    <Cart cart={cart}></Cart>
-                </div>
-            </div>
-        </>
+    setCart(newCart);
+    // save to local storage
+    addToDb(product.key);
+  };
+  const handleSearch = (event) => {
+    const searchText = event.target.value;
+    const matchedProduct = products.filter((product) =>
+      product.name.toLowerCase().includes(searchText.toLowerCase())
     );
+    setDisplayProducts(matchedProduct);
+  };
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          onChange={handleSearch}
+          placeholder="Search Product"
+        />
+      </div>
+      <div className="shop-container">
+        <div className="product-container">
+          {/* <h3>Product: {products.length}</h3> */}
+          {displayProducts.map((product) => (
+            <Product
+              key={product.key}
+              product={product}
+              handleAddToCart={handleAddToCart}
+            ></Product>
+          ))}
+        </div>
+        <div className="cart-container">
+          <Cart cart={cart}>
+            <Link to="/review">
+              <button className="btn-regular">Review Your Order</button>
+            </Link>
+          </Cart>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Shop;
